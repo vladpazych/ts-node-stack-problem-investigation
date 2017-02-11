0515400affcecfa4e0bf4df5aa2d7109a5b9949e
Vladislav Pazych <vladpazych@gmail.com>
2017-02-11 12:40:46 +0200

    Fix typo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var testingFile = "JS";
 // 5. Look at console, find out that line info changed.
 // 6. Uncomment 'withTsNode();'.
 // 7. npm start.
-// 8. Add blank lines to fileToChangeJS.js.
+// 8. Add blank lines to fileToChangeTS.ts.
 // 9. Look at console, find out that line info DID NOT change.
 // 10. Add 10 blank lines and above 'new Error()' somewhere add '//' lines or 'console.log()'
 // 11. Observe weird line info changing when add more '//' lines above
